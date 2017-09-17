bf5fce123be3f4f1985224c8f787eccd6729681d
Apichai Chenthanakij <badchen@yahoo.com>
2017-09-17 10:56:37 -0700

    creature creation now working(creatures#new & creatures#create)

--- app/controllers/creatures_controller.rb
+++ app/controllers/creatures_controller.rb
@@ -4,4 +4,19 @@ class CreaturesController < ApplicationController
     @creatures = Creature.all
   end
 
+  def new
+  end
+
+  def create
+    creature = Creature.create(creature_params)
+    redirect_to creatures_path
+
+  end
+
+  private
+
+  def creature_params
+    params.require(:creature).permit(:name, :description)
+  end
+
 end
